cdbb6315ead902e3fa7ca1d1cbbd0c396cc22517
Finn Terdal <finn@Finns-MacBook-Pro.local>
2021-01-18 16:14:24 -0600

    vanillaJS 1

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
+// import { fromEvent } from "rxjs";
+
 const btnContainer = document.querySelector("#btn-container");
+const currentColor = document.querySelector("#current-color");
 const blueBtn = document.querySelector(".btn .blue");
 const yellowBtn = document.querySelector(".btn .yellow");
 const greenBtn = document.querySelector(".btn .green");
@@ -8,14 +11,23 @@ const body = document.querySelector("body");
 // Click Event: Change Background Color
 btnContainer.addEventListener("click", function (event) {
   const color = event.target.dataset.color;
+  body.className = "";
   if (color) {
-    body.className = "";
     body.classList.add(color);
+  } else {
+    body.classList.add("white");
   }
 });
 
 // Click Event: Change Current Color
-// TODO
+btnContainer.addEventListener("click", function (event) {
+  const color = event.target.dataset.color;
+  if (color) {
+    currentColor.innerHTML = color[0].toUpperCase() + color.slice(1);
+  } else {
+    currentColor.innerHTML = "White";
+  }
+});
 
 // Click Event: Append to Past Colors
 // TODO
