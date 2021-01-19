9765482ae99eb1790649042f4fb34f90e9f30e1f
Finn Terdal <finn@Finns-MacBook-Pro.local>
2021-01-18 20:44:51 -0600

    generate random sequence with no two same in a row

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,32 @@
 import { fromEvent, interval } from "rxjs";
 
+function pickRandomElement(array: any[]) {
+  const idx = Math.floor(Math.random() * array.length);
+  return array[idx];
+}
+
+const validColors: string[] = ["blue", "yellow", "green", "red"];
+
+function generateArrayOfColors(num = 5): string[] {
+  const availableColors: string[] = ["blue", "yellow", "green", "red"];
+  let colors: string[];
+  for (let i = 0; i < num; i++) {
+    if (!colors) {
+      const newColor = pickRandomElement(availableColors);
+      colors = [newColor];
+    } else {
+      const previousColor = colors[i - 1];
+      const filteredColors = availableColors.filter(
+        (color) => color !== previousColor
+      );
+      const newColor = pickRandomElement(filteredColors);
+      colors.push(newColor);
+    }
+  }
+  return colors;
+}
+console.log(generateArrayOfColors());
+
 const btnContainer = document.querySelector("#btn-container");
 const currentColor = document.querySelector("#current-color");
 const body = document.querySelector("body");
